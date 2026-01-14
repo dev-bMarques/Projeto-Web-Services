5657b6e416b644408d92317af0b338e55191431f
Bruno Marques <dev.b.marques@gmail.com>
2026-01-14 18:26:26 -0300

    adição do profile DEV, tratamento de exceções, métodos e entidades

--- src/main/java/com/b/dev/webServices/entities/Order.java
+++ src/main/java/com/b/dev/webServices/entities/Order.java
@@ -40,7 +40,7 @@ public class Order implements Serializable {
 
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
+
 	public Order() {
 	}
 
@@ -93,11 +93,19 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-	
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
